84548b814672b5f26c8a585d22226d0d9b788d65
Przemek Kitszel <przemyslaw.kitszel@intel.com>
2024-10-31 16:44:16 +0100

    wip-dlugi-weeknd

--- drivers/net/ethernet/intel/ice/devlink/resource.h
+++ drivers/net/ethernet/intel/ice/devlink/resource.h
@@ -1,8 +1,10 @@
 struct ice_pf;
+struct ice_vf;
 
 void ice_devlink_whole_dev_resources_register(struct device *dev,
 					      struct devlink *devlink);
 void ice_devl_pf_resources_register(struct ice_pf *pf);
+void ice_devlink_vf_resources_register(struct ice_vf *vf);
 
 int ice_take_rss_lut_pf(struct ice_pf *pf, void *owner);
 int ice_take_rss_lut_global(struct ice_pf *pf, void *owner);
