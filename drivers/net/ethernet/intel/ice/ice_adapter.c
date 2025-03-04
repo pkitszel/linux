64595617f6fe6bbe6ac8211f9b1b9efc48ec5b83
Przemek Kitszel <przemyslaw.kitszel@intel.com>
2025-03-04 13:52:20 +0100

    ice: use DSN instead of PCI BDF for ice_adapter index
    Use Device Serial Number instead of PCI bus/device/function for
    index of struct ice_adapter.
    Functions on the same physical device should point to the very same
    ice_adapter instance.
    
    This is not only simplification, but also fixes things up when PF
    is passed to VM (and thus has a random BDF).
    
    Suggested-by: Jacob Keller <jacob.e.keller@intel.com>
    Suggested-by: Jakub Kicinski <kuba@kernel.org>
    Suggested-by: Jiri Pirko <jiri@resnulli.us>
    Reviewed-by: Aleksandr Loktionov <aleksandr.loktionov@intel.com>
    Signed-off-by: Przemek Kitszel <przemyslaw.kitszel@intel.com>
    ---
    CC: Karol Kolacinski <karol.kolacinski@intel.com>
    CC: Grzegorz Nitka <grzegorz.nitka@intel.com>
    CC: Michal Schmidt <mschmidt@redhat.com>
    CC: Sergey Temerkhanov <sergey.temerkhanov@intel.com>
    
    v2:
     - target to -net (Jiri)
     - mix both halves of u64 DSN on 32bit systems (Jiri)
     - (no changes in terms of fallbacks for pre-prod HW)
     - warn when there is DSN collision after reducing to 32bit
    
    v1:
    https://lore.kernel.org/netdev/20250306211159.3697-2-przemyslaw.kitszel@intel.com

--- drivers/net/ethernet/intel/ice/ice_adapter.c
+++ drivers/net/ethernet/intel/ice/ice_adapter.c
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-only
 // SPDX-FileCopyrightText: Copyright Red Hat
 
-#include <linux/bitfield.h>
 #include <linux/cleanup.h>
 #include <linux/mutex.h>
 #include <linux/pci.h>
@@ -14,29 +13,13 @@
 static DEFINE_XARRAY(ice_adapters);
 static DEFINE_MUTEX(ice_adapters_mutex);
 
-/* PCI bus number is 8 bits. Slot is 5 bits. Domain can have the rest. */
-#define INDEX_FIELD_DOMAIN GENMASK(BITS_PER_LONG - 1, 13)
-#define INDEX_FIELD_DEV    GENMASK(31, 16)
-#define INDEX_FIELD_BUS    GENMASK(12, 5)
-#define INDEX_FIELD_SLOT   GENMASK(4, 0)
-
-static unsigned long ice_adapter_index(const struct pci_dev *pdev)
+static unsigned long ice_adapter_index(u64 dsn)
 {
-	unsigned int domain = pci_domain_nr(pdev->bus);
-
-	WARN_ON(domain > FIELD_MAX(INDEX_FIELD_DOMAIN));
-
-	switch (pdev->device) {
-	case ICE_DEV_ID_E825C_BACKPLANE:
-	case ICE_DEV_ID_E825C_QSFP:
-	case ICE_DEV_ID_E825C_SFP:
-	case ICE_DEV_ID_E825C_SGMII:
-		return FIELD_PREP(INDEX_FIELD_DEV, pdev->device);
-	default:
-		return FIELD_PREP(INDEX_FIELD_DOMAIN, domain) |
-		       FIELD_PREP(INDEX_FIELD_BUS,    pdev->bus->number) |
-		       FIELD_PREP(INDEX_FIELD_SLOT,   PCI_SLOT(pdev->devfn));
-	}
+#if BITS_PER_LONG == 64
+	return dsn;
+#else
+	return (u32)dsn ^ u32(dsn >> 32);
+#endif
 }
 
 static struct ice_adapter *ice_adapter_new(void)
@@ -77,17 +60,20 @@ static void ice_adapter_free(struct ice_adapter *adapter)
  * Return:  Pointer to ice_adapter on success.
  *          ERR_PTR() on error. -ENOMEM is the only possible error.
  */
-struct ice_adapter *ice_adapter_get(const struct pci_dev *pdev)
+struct ice_adapter *ice_adapter_get(struct pci_dev *pdev)
 {
-	unsigned long index = ice_adapter_index(pdev);
+	u64 dsn = pci_get_dsn(pdev);
 	struct ice_adapter *adapter;
+	unsigned long index;
 	int err;
 
+	index = ice_adapter_index(dsn);
 	scoped_guard(mutex, &ice_adapters_mutex) {
 		err = xa_insert(&ice_adapters, index, NULL, GFP_KERNEL);
 		if (err == -EBUSY) {
 			adapter = xa_load(&ice_adapters, index);
 			refcount_inc(&adapter->refcount);
+			WARN_ON_ONCE(adapter->device_serial_number != dsn);
 			return adapter;
 		}
 		if (err)
@@ -96,6 +82,7 @@ struct ice_adapter *ice_adapter_get(const struct pci_dev *pdev)
 		adapter = ice_adapter_new();
 		if (!adapter)
 			return ERR_PTR(-ENOMEM);
+		adapter->device_serial_number = dsn;
 		xa_store(&ice_adapters, index, adapter, GFP_KERNEL);
 	}
 	return adapter;
@@ -110,11 +97,13 @@ struct ice_adapter *ice_adapter_get(const struct pci_dev *pdev)
  *
  * Context: Process, may sleep.
  */
-void ice_adapter_put(const struct pci_dev *pdev)
+void ice_adapter_put(struct pci_dev *pdev)
 {
-	unsigned long index = ice_adapter_index(pdev);
+	u64 dsn = pci_get_dsn(pdev);
 	struct ice_adapter *adapter;
+	unsigned long index;
 
+	index = ice_adapter_index(dsn);
 	scoped_guard(mutex, &ice_adapters_mutex) {
 		adapter = xa_load(&ice_adapters, index);
 		if (WARN_ON(!adapter))
