ed4855fc9381eb52bf2ad19d89817fb7425d0c24
Przemek Kitszel <przemyslaw.kitszel@intel.com>
2025-07-02 12:28:28 +0200

    ice: finish ice_virtchnl.c split into ice_virtchnl_rss.c
    Move functions out of ice_virtchnl.c to ice_virtchnl_rss.c.
    
    Same "git tricks" used as for the split into ice_virtchnl_queues.c
    that is immediately preceding this split.
    
    Signed-off-by: Przemek Kitszel <przemyslaw.kitszel@intel.com>

---
+++ drivers/net/ethernet/intel/ice/ice_virtchnl_rss.h
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: GPL-2.0
+/* Copyright (C) 2022, Intel Corporation. */
+
+#ifndef _ICE_VIRTCHNL_RSS_H_
+#define _ICE_VIRTCHNL_RSS_H_
+#include "ice.h"
+
+int ice_vc_handle_rss_cfg(struct ice_vf *vf, u8 *msg, bool add);
+int ice_vc_config_rss_key(struct ice_vf *vf, u8 *msg);
+int ice_vc_config_rss_lut(struct ice_vf *vf, u8 *msg);
+int ice_vc_config_rss_hfunc(struct ice_vf *vf, u8 *msg);
+int ice_vc_get_rss_hashcfg(struct ice_vf *vf);
+int ice_vc_set_rss_hashcfg(struct ice_vf *vf, u8 *msg);
+#endif /* _ICE_VIRTCHNL_RSS_H_ */
