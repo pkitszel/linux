3faeab652ab1d5750a1519951bc9b0b913de5d6a
Przemek Kitszel <przemyslaw.kitszel@intel.com>
2025-07-16 13:02:50 +0200

    ice: add GRE and GTPC mappings for virtchnl
    Signed-off-by: Przemek Kitszel <przemyslaw.kitszel@intel.com>

--- include/linux/avf/virtchnl.h
+++ include/linux/avf/virtchnl.h
@@ -1253,6 +1253,8 @@ enum virtchnl_proto_hdr_type {
 	VIRTCHNL_PROTO_HDR_ESP,
 	VIRTCHNL_PROTO_HDR_AH,
 	VIRTCHNL_PROTO_HDR_PFCP,
+	VIRTCHNL_PROTO_HDR_GTPC,
+	VIRTCHNL_PROTO_HDR_GRE,
 };
 
 /* Protocol header field within a protocol header. */
@@ -1317,6 +1319,9 @@ enum virtchnl_proto_hdr_field {
 	VIRTCHNL_PROTO_HDR_PFCP_S_FIELD =
 		PROTO_HDR_FIELD_START(VIRTCHNL_PROTO_HDR_PFCP),
 	VIRTCHNL_PROTO_HDR_PFCP_SEID,
+	/* GTPC */
+	VIRTCHNL_PROTO_HDR_GTPC_TEID =
+		PROTO_HDR_FIELD_START(VIRTCHNL_PROTO_HDR_GTPC),
 };
 
 struct virtchnl_proto_hdr {
