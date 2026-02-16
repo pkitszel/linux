cfc3f8cf7ff4594b4b4b51a53050b5bca04941ef
Przemek Kitszel <przemyslaw.kitszel@intel.com>
2026-02-16 14:09:07 +0100

    iavf: increase max number of queues to 256
    Increase the max number of queues that driver will handle to 256.
    Use old legacy limit in the virtchnl handling of iavf_map_queues().
    
    Reviewed-by: Jedrzej Jagielski <jedrzej.jagielski@intel.com>
    Signed-off-by: Przemek Kitszel <przemyslaw.kitszel@intel.com>

--- drivers/net/ethernet/intel/iavf/iavf.h
+++ drivers/net/ethernet/intel/iavf/iavf.h
@@ -87,8 +87,10 @@ struct iavf_vsi {
 #define IAVF_TX_DESC(R, i) (&(((struct iavf_tx_desc *)((R)->desc))[i]))
 #define IAVF_TX_CTXTDESC(R, i) \
 	(&(((struct iavf_tx_context_desc *)((R)->desc))[i]))
+
 /* for "old" virtchnl opcodes that accept up to 16 queues */
 #define IAVF_MAX_REQ_QUEUES_VCV1	16
+#define IAVF_MAX_REQ_QUEUES		256
 
 #define IAVF_HKEY_ARRAY_SIZE ((IAVF_VFQF_HKEY_MAX_INDEX + 1) * 4)
 #define IAVF_HLUT_ARRAY_SIZE ((IAVF_VFQF_HLUT_MAX_INDEX + 1) * 4)
@@ -108,7 +110,7 @@ struct iavf_q_vector {
 	struct napi_struct napi;
 	struct iavf_ring_container rx;
 	struct iavf_ring_container tx;
-	u32 ring_mask;
+	DECLARE_BITMAP(ring_mask, IAVF_MAX_REQ_QUEUES);
 	u8 itr_countdown;	/* when 0 should adjust adaptive ITR */
 	u8 num_ringpairs;	/* total number of ring pairs in vector */
 	u16 v_idx;		/* index in the vsi->q_vector array. */
