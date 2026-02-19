4cbf6c6e742edc336ddb85db2b51286f5b097941
Przemek Kitszel <przemyslaw.kitszel@intel.com>
2026-02-19 23:42:41 +0100

    ice: support up to 256 VF queues
    Add support for up to 256 VF queues. To enable more than usual 16, user
    needs to assign GLOBAL LUT (devlink resource named "rss/lut_512") for 64
    queues or PF LUT (name "rss/lut_2048") for maximum of 256 queues. There is
    a need to assign the GLOBAL LUT to PF first, then release the PF LUT
    (initially assigned to PF) from PF, to finally assign it to VF, see
    examples further below. Usual default of number of CPU cores does still
    apply, but queues could be requested later by usual ethtool -L command.
    
    Add devlink instance of VF device to track RSS LUT resources under
    a respective PF devlink instance.
    
    Number of MSI-X vectors assigned to VF is orthogonal to this.
    
    Note that VF reset is deferred to service task.
    
    How to use:
    1. Up to 64 queues
       a. assign one of 16 GLOBAL LUTs to VF:
          sudo devlink resource set pci/0000:18:01.0 path rss/lut_512 size 1
       b. if more queues than the default (num of vCPU/CPU cores) are wanted:
          sudo ethtool -L $vfiface combined $more
    
    2. Up to 256 queues
       a. assign a GLOBAL LUT to PF:
          sudo devlink resource set pci/0000:18:00.0 path rss/lut_512 size 1
       b. free the PF LUT from PF:
          sudo devlink resource set pci/0000:18:00.0 path rss/lut_2048 size 0
       c. assign the PF LUT to VF:
          sudo devlink resource set pci/0000:18:01.0 path rss/lut_2048 size 1
       d. if more queues than the default (num of vCPU/CPU cores) are wanted:
          sudo ethtool -L $vfiface combined	$more
    
    3. display current RSS LUT or RSS table:
       a. see if RSS is mapped correctly (e.g. for lut_512 there are 512
          entries expected):
          ethtool -x $vfiface
       b. see what devlink devices are present:
          devlink dev show # note the "whole dev aggregate" device over your pci netdevs
       c. see PF, VF, and "whole device aggregate" resources:
          devlink resource show pci/0000:18:00.0 # PF
          devlink resource show pci/0000:18:01.0 # VF
          devlink resource show devlink_index/3 # whole dev
    
    output will look like: below, for device with 1 of PF and GLOBAL LUTs:
    pci/0000:18:00.0:
      name rss size 2 unit entry size_min 0 size_max 2 size_gran 1 dpipe_tables none
        resources:
          name lut_512 size 1 unit entry size_min 0 size_max 1 size_gran 1 dpipe_tables none
          name lut_2048 size 1 unit entry size_min 0 size_max 1 size_gran 1 dpipe_tables none
    
    Big thanks to Mateusz for working together on the whole story for long
    time! Big thanks to Alex Loktionov for pointing the reason of one nasty
    bug with the message size!
    
    Co-developed-by: Mateusz Polchlopek <mateusz.polchlopek@intel.com>
    Signed-off-by: Mateusz Polchlopek <mateusz.polchlopek@intel.com>
    Signed-off-by: Aleksandr Loktionov <aleksandr.loktionov@intel.com>
    Reviewed-by: Jedrzej Jagielski <jedrzej.jagielski@intel.com>
    Signed-off-by: Przemek Kitszel <przemyslaw.kitszel@intel.com>
    ---
    v2:
     * prevent werid cases of null ptr deref of vf->devlink (Sashiko)
     * drop getting vf ref around vf->needs_deferred_reset - it only opened a window for resource leak and was not needed anyway (Sashiko)
     * use ice_get_vf_vsi() instead of open coding it (Sashiko)

--- drivers/net/ethernet/intel/ice/devlink/resource.h
+++ drivers/net/ethernet/intel/ice/devlink/resource.h
@@ -8,7 +8,9 @@ struct devlink;
 struct ice_adapter;
 struct ice_hw;
 struct ice_pf;
+struct ice_vf;
 
+void ice_devlink_vf_resources_register(struct ice_vf *vf);
 void ice_devl_pf_resources_register(struct ice_pf *pf);
 void ice_devl_whole_dev_resources_register(const struct ice_hw *hw,
 					   struct ice_adapter *adapter);
@@ -16,5 +18,6 @@ void ice_devl_whole_dev_resources_register(const struct ice_hw *hw,
 int ice_take_rss_lut_pf(struct ice_pf *pf);
 void ice_release_rss_lut_pf(struct ice_pf *pf);
 void ice_free_rss_lut_flr(struct ice_pf *pf);
+void ice_free_rss_lut_vf(struct ice_vf *vf);
 
 #endif /* _ICE_DEVL_RESOURCE_H_ */
