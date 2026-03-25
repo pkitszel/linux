d0873fe76e5e3380ff320e13c1e27d4f9e4967f0
Przemek Kitszel <przemyslaw.kitszel@intel.com>
2026-03-25 04:06:35 +0100

    iavf: extend iavf_configure_queues() to support more queues
    Extend iavf_configure_queues() to support more than 31 queues.
    Virtchnl opcode used was already generic, but we have just not needed
    more than one message before.
    
    Add helper, iavf_max_vc_entries(), that determines how many entries we
    could fit into virtchnl message (ending by flex array member). Will be
    also used on another op later in this series.
    
    Signed-off-by: Przemek Kitszel <przemyslaw.kitszel@intel.com>
    ---
    v2:
     * fix typo (Sashiko)
     * abort iavf_configure_queues() at error (Sashiko)
     * pass NULL msg buf for reply (me)
     * adapt to iavf_poll_virtchnl_response() taking the virtchnl op
       directly, and drop the now unused iavf_match_vc_op_cb() (me)
     * report send and poll failures with dev_err(), they are not
       recoverable here (me)

--- drivers/net/ethernet/intel/iavf/iavf_virtchnl.c
+++ drivers/net/ethernet/intel/iavf/iavf_virtchnl.c
@@ -7,6 +7,11 @@
 #include "iavf_ptp.h"
 #include "iavf_prototype.h"
 
+/* how many Flex Array Member entries do fit into VC message of type *ptr */
+#define iavf_max_vc_entries(ptr, flex_member) \
+	((IAVF_MAX_AQ_BUF_SIZE - virtchnl_struct_size(ptr, flex_member, 1)) / \
+	 sizeof(ptr->flex_member[0]) + 1)
+
 /**
  * iavf_send_pf_msg
  * @adapter: adapter structure
@@ -365,6 +370,30 @@ int iavf_get_vf_ptp_caps(struct iavf_adapter *adapter)
 	return err;
 }
 
+int iavf_get_max_rss_qregion(struct iavf_adapter *adapter)
+{
+	struct iavf_arq_event_info event;
+	int err;
+	u16 len;
+
+	len = sizeof(struct virtchnl_max_rss_qregion);
+	event.buf_len = len;
+	event.msg_buf = kzalloc(len, GFP_KERNEL);
+	if (!event.msg_buf)
+		return -ENOMEM;
+
+	err = iavf_poll_virtchnl_msg(&adapter->hw, &event,
+				     VIRTCHNL_OP_GET_MAX_RSS_QREGION);
+	if (!err)
+		memcpy(&adapter->max_rss_qregion, event.msg_buf,
+		       min(event.msg_len, len));
+
+	adapter->current_op = VIRTCHNL_OP_UNKNOWN;
+
+	kfree(event.msg_buf);
+	return err;
+}
+
 /**
  * iavf_configure_queues
  * @adapter: adapter structure
@@ -375,9 +404,11 @@ void iavf_configure_queues(struct iavf_adapter *adapter)
 {
 	struct virtchnl_vsi_queue_config_info *vqci;
 	int pairs = adapter->num_active_queues;
+	struct iavf_arq_event_info event = {};
 	struct virtchnl_queue_pair_info *vqpi;
-	u32 i, max_frame;
+	int max_pairs, err = 0;
 	u8 rx_flags = 0;
+	u32 max_frame;
 	size_t len;
 
 	max_frame = LIBIE_MAX_RX_FRM_LEN(adapter->rx_rings->pp->p.offset);
@@ -397,8 +428,9 @@ void iavf_configure_queues(struct iavf_adapter *adapter)
 			adapter->current_op);
 		return;
 	}
-	adapter->current_op = VIRTCHNL_OP_CONFIG_VSI_QUEUES;
-	len = virtchnl_struct_size(vqci, qpair, pairs);
+
+	max_pairs = iavf_max_vc_entries(vqci, qpair);
+	len = virtchnl_struct_size(vqci, qpair, min(pairs, max_pairs));
 	vqci = kzalloc(len, GFP_KERNEL);
 	if (!vqci)
 		return;
@@ -407,12 +439,11 @@ void iavf_configure_queues(struct iavf_adapter *adapter)
 		rx_flags |= VIRTCHNL_PTP_RX_TSTAMP;
 
 	vqci->vsi_id = adapter->vsi_res->vsi_id;
-	vqci->num_queue_pairs = pairs;
 	vqpi = vqci->qpair;
-	/* Size check is not needed here - HW max is 16 queue pairs, and we
-	 * can fit info for 31 of them into the AQ buffer before it overflows.
-	 */
-	for (i = 0; i < pairs; i++) {
+
+	for (int i = 0, in_msg = 0; i < pairs; i++) {
+		const bool last = i + 1 == pairs;
+
 		vqpi->txq.vsi_id = vqci->vsi_id;
 		vqpi->txq.queue_id = i;
 		vqpi->txq.ring_len = adapter->tx_rings[i].count;
@@ -430,11 +461,41 @@ void iavf_configure_queues(struct iavf_adapter *adapter)
 						   NETIF_F_RXFCS);
 		vqpi->rxq.flags = rx_flags;
 		vqpi++;
+		in_msg++;
+		if (last || in_msg == max_pairs) {
+			adapter->current_op = VIRTCHNL_OP_CONFIG_VSI_QUEUES;
+			vqci->num_queue_pairs = in_msg;
+
+			err = iavf_send_pf_msg(adapter,
+					       VIRTCHNL_OP_CONFIG_VSI_QUEUES,
+					       (u8 *)vqci,
+					       virtchnl_struct_size(vqci, qpair, in_msg));
+			if (err) {
+				dev_err(&adapter->pdev->dev,
+					 "sending VC msg to PF failed, err: %d\n",
+					 err);
+				break;
+			}
+
+			err = iavf_poll_virtchnl_response(adapter, &event,
+							  VIRTCHNL_OP_CONFIG_VSI_QUEUES,
+							  NULL, NULL, 1000);
+			if (err) {
+				dev_err(&adapter->pdev->dev,
+					 "config queues poll failed, err: %d\n",
+					 err);
+				break;
+			}
+
+			vqpi = vqci->qpair;
+			in_msg = 0;
+		}
 	}
 
-	adapter->aq_required &= ~IAVF_FLAG_AQ_CONFIGURE_QUEUES;
-	iavf_send_pf_msg(adapter, VIRTCHNL_OP_CONFIG_VSI_QUEUES,
-			 (u8 *)vqci, len);
+	if (!err)
+		adapter->aq_required &= ~IAVF_FLAG_AQ_CONFIGURE_QUEUES;
+
+	adapter->current_op = VIRTCHNL_OP_UNKNOWN;
 	kfree(vqci);
 }
 
