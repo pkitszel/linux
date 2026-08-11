5e05863ea91fc7c196a1b82ac34f8bbde67b243c
Przemek Kitszel <przemyslaw.kitszel@intel.com>
2026-08-11 15:10:13 +0200

    Revert "ice: remove unused init_param"
    This reverts commit d5851bf105520fba1fea353d2738194408915e98.

--- drivers/net/ethernet/intel/ice/ice_adapter.c
+++ drivers/net/ethernet/intel/ice/ice_adapter.c
@@ -35,7 +35,7 @@ static u64 ice_adapter_index(struct pci_dev *pdev)
 	}
 }
 
-static int ice_adapter_init(void *priv)
+static int ice_adapter_init(void *priv, void *init_param)
 {
 	struct ice_adapter *adapter = priv;
 	struct devlink *devlink;
@@ -92,7 +92,7 @@ struct ice_adapter *ice_adapter_get(struct pci_dev *pdev)
 	index = ice_adapter_index(pdev);
 	snprintf(devlink_id, sizeof(devlink_id), "%llx", index);
 	devlink = devlink_shd_get(devlink_id, &ice_adapter_devlink_ops,
-				  sizeof(*adapter), pdev->dev.driver);
+				  sizeof(*adapter), NULL, pdev->dev.driver);
 	if (IS_ERR(devlink))
 		return ERR_CAST(devlink);
 
